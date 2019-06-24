7b73cb921090772bd2308dd5f35c29fb7ad2093e
Anton Chaynikov <anton.chaynikov@flatstack.com>
2019-06-24 18:00:39 +0300

    Fixed bug, when updated version file wouldn't be pushed to the remote

--- src/main/kotlin/com/antonchaynikov/oneclickpublish/deployment/StagePushToRemote.kt
+++ src/main/kotlin/com/antonchaynikov/oneclickpublish/deployment/StagePushToRemote.kt
@@ -10,7 +10,7 @@ class StagePushToRemote(
 
     override fun execute() {
         // pushing tag named as projectVersion.getTagName() to the remote
-        val pushCommand = ShellCommand(project.rootDir, "git", "push", remoteName, projectVersion.getTagName()).execute()
+        val pushCommand = ShellCommand(project.rootDir, "git", "push", remoteName, ":", projectVersion.getTagName()).execute()
         if (!pushCommand.isSuccess()) {
             throw Exception("Failed to push changes to the remote")
         }
